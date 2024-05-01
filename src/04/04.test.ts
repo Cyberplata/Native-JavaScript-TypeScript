5f13369aa49e6b0abb17806f02e58dfb049f246e
PlatonKynin <kininplaton@gmail.com>
2024-05-01 23:14:30 +0300

    04-filter -> фильтер completedTasks

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -20,3 +20,17 @@ test("should take courses chipper 160", () => {
     expect(chipCourses[0].title).toBe("CSS");
     expect(chipCourses[1].title).toBe("REACT");
 })
+
+test("get only completed tasks", () => {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Solt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true},
+    ]
+
+    const completedTasks = tasks.filter( task => task.isDone);
+    expect(completedTasks.length).toBe(2);
+    expect(completedTasks[0].id).toBe(2);
+    expect(completedTasks[1].id).toBe(4);
+})
