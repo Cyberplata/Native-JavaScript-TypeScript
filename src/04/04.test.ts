14df91ccb7b1aca6907ec56304a9425fa5f63a4f
PlatonKynin <kininplaton@gmail.com>
2024-05-01 23:17:57 +0300

    04-filter -> фильтер uncompletedTasks

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -29,10 +29,8 @@ test("get only completed tasks", () => {
         {id: 4, title: 'Sugar', isDone: true},
     ]
 
-    const completedTasks = tasks.filter( function (task) {
-      return task.isDone
-    });
-    expect(completedTasks.length).toBe(2);
-    expect(completedTasks[0].id).toBe(2);
-    expect(completedTasks[1].id).toBe(4);
+    const uncompletedTasks = tasks.filter( task => !task.isDone);
+    expect(uncompletedTasks.length).toBe(2);
+    expect(uncompletedTasks[0].id).toBe(1);
+    expect(uncompletedTasks[1].id).toBe(3);
 })
