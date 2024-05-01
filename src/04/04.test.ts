4e7fb776c29b7cd7efe457dd99b2f008a7e72ea2
PlatonKynin <kininplaton@gmail.com>
2024-05-01 23:06:53 +0300

    04-filter -> фильтер v2 короткая форма записи

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -1,11 +1,7 @@
 test("should take old men older then 90", () => {
     const ages = [18, 20, 22, 1, 100, 90, 14];
 
-    const predicate = (age: number) => {
-        return age > 90;
-    }
-
-    const oldAges = ages.filter(predicate);
+    const oldAges = ages.filter(age => age > 90);
 
     expect(oldAges.length).toBe(1);
     expect(oldAges[0]).toBe(100);
@@ -18,11 +14,7 @@ test("should take courses chipper 160", () => {
         {title: "REACT", price: 150}
     ]
 
-    const chipPredicate = (course: CourseType) => {
-        return course.price < 160;
-    }
-
-    const chipCourses = courses.filter(chipPredicate);
+    const chipCourses = courses.filter(course => course.price < 160);
 
     expect(chipCourses.length).toBe(2);
     expect(chipCourses[0].title).toBe("CSS");
