9420b04abd7a0b932436c139d574fa11482753f7
PlatonKynin <kininplaton@gmail.com>
2024-05-02 23:22:37 +0300

    05-map -> App.tsx - пример использования map в массиве names и объекте users вместе с index

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const names = ["Dimych", "Sveta", "Katay", "Viktor", "Ignat"]
+
+    const users = [{name: "Dimych"}, {name: "Sveta"}, {name: "Katay"}, {name: "Viktor"}, {name: "Ignat"}]
+
+    const liElements = users.map((u, index) => <li key={index}>{index} - {u.name}</li>)
+
+    return (
+        <div className="App">
+            <ul>
+                {liElements}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
