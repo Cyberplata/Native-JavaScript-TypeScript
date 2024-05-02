fc89b7556eb4d07c6a4fa1fbedca49f5371cc86d
PlatonKynin <kininplaton@gmail.com>
2024-05-02 23:24:50 +0300

    05-map -> App.tsx - пример использования map в массиве names и объекте users без index, но с id

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ function App() {
 
     const names = ["Dimych", "Sveta", "Katay", "Viktor", "Ignat"]
 
-    const users = [{name: "Dimych"}, {name: "Sveta"}, {name: "Katay"}, {name: "Viktor"}, {name: "Ignat"}]
+    const users = [
+        {id: 1, name: "Dimych"},
+        {id: 2, name: "Sveta"},
+        {id: 3, name: "Katay"},
+        {id: 4, name: "Viktor"},
+        {id: 5, name: "Ignat"}]
 
-    const liElements = users.map((u, index) => <li key={index}>{index} - {u.name}</li>)
+    const liElements = users.map(u => <li key={u.id}>{u.name}</li>)
 
     return (
         <div className="App">
