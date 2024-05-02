4d2bb23a83c0ae176ea4a554d40450a04b52b361
PlatonKynin <kininplaton@gmail.com>
2024-05-02 20:23:37 +0300

    05-map -> 05_01.ts функция dimychTransformator с return

---
+++ src/05/05_01.ts
@@ -0,0 +1,48 @@
+export type ManType = {
+    name: string
+    age: number
+}
+
+const people: Array<ManType> = [
+    { name: "Andrew Ivanov", age: 33 },
+    { name: "Alexander Petrov", age: 24 },
+    { name: "Dmitry Sidorov", age: 18 },
+]
+
+
+
+const dimychTransformator = (man: ManType) => {
+    return {
+        stack: ["css", "html", "js", "tdd", "react"],
+        firstName: man.name.split(" ")[0],
+        lastName: man.name.split(" ")[1]
+    }
+}
+
+const devs1 = [
+    {
+        stack: ["css", "html", "js", "tdd", "react"],
+        firstName: "Andrew", lastName: "Ivanov"
+    },
+    {
+        stack: ["css", "html", "js", "tdd", "react"],
+        firstName: "Alexander", lastName: "Petrov"
+    },
+    {
+        stack: ["css", "html", "js", "tdd", "react"],
+        firstName: "Dmitry", lastName: "Sidorov"
+    },
+]
+// let d1 = dimychTransformator( people[0] )
+// let d2 = dimychTransformator( people[1] )
+// let d3 = dimychTransformator( people[2] )
+
+
+const devs2 = [
+    dimychTransformator(people[0]),
+    dimychTransformator(people[1]),
+    dimychTransformator(people[2])
+]
+
+const devs3 = people.map(dimychTransformator)
+
