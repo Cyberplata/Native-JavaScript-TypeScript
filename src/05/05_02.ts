9da32a1284503cdc9ccaa2ee1bee15f55a212429
PlatonKynin <kininplaton@gmail.com>
2024-05-05 09:06:39 +0300

    06-callbacks -> App.tsx - пример использования callback в файле 06_callbacks.ts -> setTimeout

--- src/05/05_02.ts
+++ src/05/05_02.ts
@@ -1,14 +1,17 @@
 import {GovernmentBuildingType, HouseType} from "../02/02_02";
 
 export const getStreetsTitlesOfGovernmentBuildings = (buildings: Array<GovernmentBuildingType>) => {
-    return buildings.map( b => b.address.street.title )
+    return buildings.map(b => b.address.street.title)
 }
 
 export const getStreetsTitlesOfHouses = (houses: Array<HouseType>) => {
-    return houses.map( h => h.address.street.title )
+    return houses.map(h => h.address.street.title)
 }
 
 export function createMessage(houses: Array<HouseType>) {
-    let callbackfn = (h: HouseType) => `Hello guys from ${h.address.street.title}`;
-    return houses.map( callbackfn)
+    let callbackfn = (h: HouseType) => {
+        return `Hello guys from ${h.address.street.title}`;
+    }
+    let newArray = houses.map(callbackfn);
+    return newArray
 }
