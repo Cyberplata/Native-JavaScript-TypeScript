fc744d54fbaeefaa51fad77ad55cbe667277eb0d
PlatonKynin <kininplaton@gmail.com>
2024-05-05 14:53:52 +0300

    07 - Деструктурирующее присваивание. destructuring.test.ts -> деструктуризация {age, lessons}

--- src/07_Destructuring-assignment/destructuring.test.ts
+++ src/07_Destructuring-assignment/destructuring.test.ts
@@ -7,11 +7,20 @@ test('', ()=> {
         lessons: [{title: "1"}, {title: "2"}]
     }
 
-    const age = props.age
-    const lessons = props.lessons
+    // const age = props.age
+    // const lessons = props.lessons
+
+    const {age, lessons} = props // то же самое, что написано сверху
+
+    const a = props.age
+    const l = props.lessons
 
 
     expect(age).toBe(32)
     expect(lessons.length).toBe(2)
+
+
+    expect(a).toBe(32)
+    expect(l.length).toBe(2)
 
 })
