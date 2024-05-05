a241d323653bc986980f0c9bc1d2f8b14553487a
PlatonKynin <kininplaton@gmail.com>
2024-05-05 13:04:05 +0300

    06-callbacks -> 06-callbacks.tsx пример с search в кнопке вместо deleteUser. Но теперь проблема прочитать value, которое ввели в input

--- src/06/06-callbacks.tsx
+++ src/06/06-callbacks.tsx
@@ -1,21 +1,23 @@
-import React, {MouseEvent} from 'react'
+import React, {ChangeEvent, MouseEvent} from 'react'
 
 export const User = () => {
-    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
-        alert(event.currentTarget.name);
+    const search = (event: MouseEvent<HTMLButtonElement>) => {
+        event.currentTarget.name
     }
 
     const onNameChanged = () => {
         console.log("name changed")
+    }
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement> ) => {
+        console.log("age changed" + event.currentTarget.value)
     }
     const focusLostHandler = () => {
         console.log('focus lost')
     }
 
     return <div><textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
-        <input type="text"/>
+        <input onChange={onAgeChanged} type={"number"}/>
 
-        <button name="delete" onClick={deleteUser}>delete</button>
-        <button name="save" onClick={deleteUser} >save</button>
+        <button name="search" onClick={search}>search</button>
     </div>
 }
