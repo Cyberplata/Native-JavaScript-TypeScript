7902a62062620398dcdd36780ca056d77d2d6237
PlatonKynin <kininplaton@gmail.com>
2024-05-19 18:32:13 +0300

    08.1_Associative-Array-> мгновенный поиск через [] -> mentor1["my-salary"]

--- src/08_Working-array/array.js
+++ src/08_Working-array/array.js
@@ -1,8 +1,8 @@
     const mentors = [
-        {id: 'id100', title: "Dimych", salary: 10}, //0
-        {id: 'id20222', title: "Viktor", salary: 100}, //1
-        {id: 'id323242', title: "Valera", salary: 100}, //2
-        {id: 'id489829898989', title: "Artem", salary: 100}, //3
+        {id: 'id100', title: "Dimych", "my-salary": 10}, //0
+        {id: 'id20222', title: "Viktor", "my-salary": 100}, //1
+        {id: 'id323242', title: "Valera", "my-salary": 100}, //2
+        {id: 'id489829898989', title: "Artem", "my-salary": 100}, //3
     ];
 
     const artemId = 'id489829898989';
@@ -10,12 +10,12 @@
     const mentor1 = mentors.find(m => m.id === artemId);
     const mentor2 = mentors[3];
 
-    let keys = Object.keys(mentor1);
-    console.log(keys)
-    mentor1.salary = 50;
-    // console.log(mentors)
-    // console.log(mentor1)
-    // console.log(mentor2)
+    // let keys = Object.keys(mentor1);
+    // console.log(keys)
+    mentor1["my-salary"] = 50;
+    console.log(mentor1)
+
+
 
     const students = [
         {id: 'id100', title: "Andrew"}, //0
