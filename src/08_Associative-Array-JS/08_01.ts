dde422ffe1fc15be381edefab87798bf9eef849f
PlatonKynin <kininplaton@gmail.com>
2024-05-20 23:06:40 +0300

    08.2_Associative-Array -> test: toBe+toEqual+toBeUndefined

--- src/08_Associative-Array-JS/08_01.ts
+++ src/08_Associative-Array-JS/08_01.ts
@@ -18,16 +18,7 @@ export const usersObj = {
     '3': 'Katya',
 }
 
-type UsersType = {
-    [key: string]: {id: number, name: string}
-}
 
-export const users: UsersType = {
-    '101': {id: 101, name: 'Dimych'},
-    '3232312': {id: 3232312, name: 'Natasha'},
-    '1212': {id: 1212, name: 'Valera'},
-    '1': {id: 1, name: 'Katya'},
-}
 // users[1] //O(1)
 
 let user = {id: 100500, name: 'Igor'};
