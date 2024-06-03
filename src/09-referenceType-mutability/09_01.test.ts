874e31a6b2ab930feb0f0a18d71f46a1f2943d90
PlatonKynin <kininplaton@gmail.com>
2024-06-03 22:16:22 +0300

    09-Reference type, mutability, мутации->array test method push

--- src/09-referenceType-mutability/09_01.test.ts
+++ src/09-referenceType-mutability/09_01.test.ts
@@ -23,16 +23,18 @@ test('reference type test', () => {
 })
 
 test('array test', () => {
+    let users = [
+        {
+            name: 'Dimych',
+            age: 32
+        },
+        {
+            name: 'Dimych',
+            age: 32
+        }
+    ]
+    let admins = users
+    admins.push({name: 'Bandyugan', age: 10})
 
-    let user = {
-        name: 'Dimych',
-        age: 32
-    }
-    icreaseAge(user)
-
-    expect(user.age).toBe(33)
-
-    const superman = user
-    superman.age = 1000
-    expect(user.age).toBe(1000)
+    expect(users[2]).toEqual({name: 'Bandyugan', age: 10})
 })
