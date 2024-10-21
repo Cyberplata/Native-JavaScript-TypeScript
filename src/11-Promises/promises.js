05d337636e1435081f7e1cd228cb7b8a61a14d8a
PlatonKynin <kininplaton@gmail.com>
2024-10-21 23:35:44 +0300

    11-Promises-01-1.Что такое promise, then, catch - основы

--- src/11-Promises/promises.js
+++ src/11-Promises/promises.js
@@ -12,28 +12,50 @@ const axios = {
 }
 
 const findUserInDB = (id) => {
-    // id: {id: 1, name: "Dimych"},
+    // const obj = {
+    //     [id]: {id: 1, name: "Dimych"},
+    //     [id]: {id: 2, name: "Sveta"},
+    // }
+    // return obj
+
+    return [
+        {[id]: 1, name: "Dimych"},
+        {[id]: 2, name: "Sveta"},
+    ]
 }
+
 // Что такое promise, then, catch - основы
-//1. Длинный синтаксис
-const promise1 = axios.get("https://google.com")
-promise1.then((data) => {
-    console.log(data)
-})
-//1.1 Короткий синтаксис
-axios.get("https://google.com")
-    .then((data) => {
-        console.log(data)
+// //1. Длинный синтаксис
+// const promise1 = axios.get("https://google.com")
+// promise1.then((data) => {
+//     console.log(data)
+// })
+// //1.1 Короткий синтаксис
+// axios.get("https://google.com")
+//     .then((data) => {
+//         console.log(data)
+//     })
+
+// //2 Длинный синтаксис
+// const promises2 = findUserInDB(1)
+// promises2.then((user) => {
+//     console.log(user)
+// })
+// console.log('finish')
+
+// //2.1 Короткий синтаксис
+// findUserInDB(1).then((user) => {
+//     console.log(user)
+// })
+
+const promises2 = findUserInDB(2)
+promises2
+    .then((user) => {
+        console.log(user)
+        console.log('fininsh')
     })
-
-//2 Длинный синтаксис
-const promises2 = findUserInDB(1)
-promises2.then((user) => {
-    console.log(user)
-})
-console.log('finish')
-
-//2.1 Короткий синтаксис
-findUserInDB(1).then((user) => {
-    console.log(user)
-})
+    .catch((error) => {
+        console.warn(error)
+        console.log('fininsh')
+    })
+// 32:02
