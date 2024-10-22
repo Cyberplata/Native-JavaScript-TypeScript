032bf588bda9285cf0e1d22485a125aba8006b0c
PlatonKynin <kininplaton@gmail.com>
2024-10-22 22:01:04 +0300

    11-Promises-01-2. axios, findUserInDB, randomIntFromInterval function

--- src/11-Promises/promises.js
+++ src/11-Promises/promises.js
@@ -1,27 +1,64 @@
 const axios = {
-    request: {},
-    status: 200,
-    headers: {},
-    config: {},
-    data: {
-        vacancies: 12,
+    _fake(url, data) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let responseData = {
+                    text: `${url} loves you`
+                };
+                if (url.indexOf('it-kamasutra') > 0) {
+                    responseData = {
+                        requestedCount: data.count,
+                        message: 'we will prepare students for you'
+                    }
+                } else if (url.indexOf('google') > 0) {
+                    responseData = {
+                        vacancies: 12,
+                    }
+                } else if (url.indexOf('microsoft') > 0) {
+                    responseData = {
+                        vacancies: 21,
+                    }
+                }
+                resolve({
+                    request: {},
+                    status: 200,
+                    headers: {},
+                    config: {},
+                    data: responseData
+                })
+            }, randomIntFromInterval(1, 5) * 1000);
+        });
     },
-    get() {
-        console.log("google!!!")
+    
+    post(url, data) {
+        return this._fake(url, data);
+    },
+    
+    get(url, data) {
+        return this._fake(url, data);
     }
 }
 
 const findUserInDB = (id) => {
-    // const obj = {
-    //     [id]: {id: 1, name: "Dimych"},
-    //     [id]: {id: 2, name: "Sveta"},
-    // }
-    // return obj
-
-    return [
-        {[id]: 1, name: "Dimych"},
-        {[id]: 2, name: "Sveta"},
+    const users = [
+        {id: 1, name: "Dimych", friend: 3},
+        {id: 2, name: "Sveta", friend: null},
+        {id: 3, name: "Sveta", friend: 2}
     ]
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            let user = users.find(u => u.id === id)
+            if (user === null) {
+                rej('user not found')
+            } else {
+                res(user)
+            }
+        }, randomIntFromInterval(500, 1000))
+    })
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // Что такое promise, then, catch - основы
@@ -49,13 +86,17 @@ const findUserInDB = (id) => {
 // })
 
 const promises2 = findUserInDB(2)
+console.log(promises2)
 promises2
     .then((user) => {
-        console.log(user)
-        console.log('fininsh')
+        console.log(promises2)
+        // console.log(user)
     })
     .catch((error) => {
         console.warn(error)
+    })
+    .finally(() => {
         console.log('fininsh')
     })
 // 32:02
+console.log('!!!!')
